3fbb565f9ae96067bc5121f3bf05f1d94beece32
belindaabusk <au615813@post.au.dk>
2020-05-11 16:28:04 +0200

    Tilføjet EKG char og sygdomsalgoritme til ekgvinduet

--- Projektet/EKGOversigten.xaml.cs
+++ Projektet/EKGOversigten.xaml.cs
@@ -18,104 +18,113 @@ using LogicLayer;
 
 namespace Projektet_GUI
 {
-   /// <summary>
-   /// Interaction logic for EKGOversigten.xaml
-   /// </summary>
+    /// <summary>
+    /// Interaction logic for EKGOversigten.xaml
+    /// </summary>
 
-   public partial class EKGOversigten : Window
-   {
-      private List<Patient> PatientListe;
-      private Logic logicref;
-      private List<EKG_Maaling> MaalingListe;
-      private EKG_Maaling Maaling;
-      private Patient patient1;
+    public partial class EKGOversigten : Window
+    {
+        public ChartValues<double> Ekgvalues { get; set; }
+        private List<Patient> PatientListe;
+        private Logic logicref;
+        private List<EKG_Maaling> MaalingListe;
+        private EKG_Maaling Maaling;
+        private Patient patient1;
 
-      public EKGOversigten(Logic logicref)
-      {
-         InitializeComponent();
-         this.logicref = logicref;
-      }
+        public EKGOversigten(Logic logicref)
+        {
+            InitializeComponent();
+            this.logicref = logicref;
+        }
 
-      private void Window_Loaded(object sender, RoutedEventArgs e)
-      {
-         PatientListe = logicref.getPatientListe();
-         foreach (Patient item in PatientListe)
-         {
-
-            PatientLB.Items.Add(item.Navn + " " + item.Efternavn);
-         }
-
-         fnTB.Text = "Søren";
-         EfTB.Text = "Pedersen";
-         MedarnrTB.Text = "12435687";
-      }
-
-      private void GemB_Click(object sender, RoutedEventArgs e)
-      {
-         MaalingListe = logicref.getMaalingListe(PatCprTB.Text);
-         patient1 = logicref.getCPR(PatCprTB.Text);
-         int ekgmaaleid;
-         int antalmaalinger = 0;
-         foreach (EKG_Maaling item in MaalingListe)
-         {
-            antalmaalinger++;
-            if (Convert.ToDateTime(DatoLB.SelectedItem) == item.DateTime)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PatientListe = logicref.getPatientListe();
+            foreach (Patient item in PatientListe)
             {
-               ekgmaaleid = item.id;
-               logicref.gemIoffentligDatabase(ekgmaaleid, Convert.ToDateTime(DatoLB.SelectedItem), antalmaalinger, fnTB.Text, EfTB.Text, Convert.ToInt32(MedarnrTB.Text), orgTB.Text, KommentarTB.Text, patient1.Navn, patient1.Efternavn, PatCprTB.Text);
-            }
-         }
-         //logicref.gemIoffentligDatabase(ekgmaaleid, Convert.ToDateTime(DatoLB.SelectedItem), "", fnTB.Text, EfTB.Text, Convert.ToInt32(MedarnrTB.Text), orgTB.Text, KommentarTB.Text, "", "", PatientInfoTB.Text, PatCprTB.Text);
-         //uploadet alle informationer til EGKmålinger og EKGData (offentlige)
-      }
 
-      private void PatientLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
-      {
-         foreach (Patient item in PatientListe)
-         {
-            if (Convert.ToString(PatientLB.SelectedItem) == item.Navn + " " + item.Efternavn)
+                PatientLB.Items.Add(item.Navn + " " + item.Efternavn);
+            }
+
+            fnTB.Text = "Søren";
+            EfTB.Text = "Pedersen";
+            MedarnrTB.Text = "12435687";
+        }
+
+        private void GemB_Click(object sender, RoutedEventArgs e)
+        {
+            MaalingListe = logicref.getMaalingListe(PatCprTB.Text);
+            patient1 = logicref.getCPR(PatCprTB.Text);
+            int ekgmaaleid;
+            int antalmaalinger = 0;
+            foreach (EKG_Maaling item in MaalingListe)
             {
-               PatCprTB.Text = item.CPR;
+                antalmaalinger++;
+                if (Convert.ToDateTime(DatoLB.SelectedItem) == item.DateTime)
+                {
+                    ekgmaaleid = item.id;
+                    logicref.gemIoffentligDatabase(ekgmaaleid, Convert.ToDateTime(DatoLB.SelectedItem), antalmaalinger, fnTB.Text, EfTB.Text, Convert.ToInt32(MedarnrTB.Text), orgTB.Text, KommentarTB.Text, patient1.Navn, patient1.Efternavn, PatCprTB.Text);
+                }
             }
-         }
+            //logicref.gemIoffentligDatabase(ekgmaaleid, Convert.ToDateTime(DatoLB.SelectedItem), "", fnTB.Text, EfTB.Text, Convert.ToInt32(MedarnrTB.Text), orgTB.Text, KommentarTB.Text, "", "", PatientInfoTB.Text, PatCprTB.Text);
+            //uploadet alle informationer til EGKmålinger og EKGData (offentlige)
+        }
 
-         MaalingListe = logicref.getMaalingListe(PatCprTB.Text);
+        private void PatientLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (Patient item in PatientListe)
+            {
+                if (Convert.ToString(PatientLB.SelectedItem) == item.Navn + " " + item.Efternavn)
+                {
+                    PatCprTB.Text = item.CPR;
+                }
+            }
 
-         foreach (EKG_Maaling item in MaalingListe)
-         {
-            DatoLB.Items.Add(item.DateTime);
-         }
+            MaalingListe = logicref.getMaalingListe(PatCprTB.Text);
 
-      }
+            foreach (EKG_Maaling item in MaalingListe)
+            {
+                DatoLB.Items.Add(item.DateTime);
+            }
 
-      private void DatoLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
-      {
-         foreach (EKG_Maaling item in MaalingListe)
-         {
-            if (item.DateTime == Convert.ToDateTime(DatoLB.SelectedItem))
-               Maaling = logicref.sygdomsalgoritme_Måling(item.CPR, item.DateTime);
-         }
+        }
 
-            SeriesCollection ekgcollection = new SeriesCollection();
-            ChartValues<double> ekgvalues = new ChartValues<double>();
+        private void DatoLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (EKG_Maaling item in MaalingListe)
+            {
+                if (item.DateTime == Convert.ToDateTime(DatoLB.SelectedItem))
+                    Maaling = logicref.sygdomsalgoritme_Måling(item.CPR, item.DateTime);
+            }
 
-            
-
-         //Kode for at få et ekg op på charen - Det under fungere ikke - i skal selv implementere charen  
-         foreach (double item in Maaling.EKG_Data)
-         {
-                ekgvalues.Add(item);
-         }
-
-         //Dette er en bool alt efter om der er dedekteret sygdom eller ej - skriv eller ring hvis i mangler noget :D 
-
-
-         //Kode for at få et ekg op på charen  
+            Ekgvalues = new ChartValues<double>();
+            DataContext = this;
 
 
-            
-      }
+            //Kode for at få et ekg op på charen - Det under fungere ikke - i skal selv implementere charen  
+            foreach (double item in Maaling.EKG_Data)
+            {
+                Ekgvalues.Add(item);
+            }
 
-   }
+            if (Maaling.Sygdom == true)
+            {
+                sygdomTB.Text = "Ja";
+            }
+            else
+            {
+                sygdomTB.Text = "Nej";
+            }
+
+            //Dette er en bool alt efter om der er dedekteret sygdom eller ej - skriv eller ring hvis i mangler noget :D 
+
+
+            //Kode for at få et ekg op på charen  
+
+
+
+        }
+
+    }
 
 }
