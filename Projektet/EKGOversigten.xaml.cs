520b74c67cab3a8a975e6a35b2342d4f5e87b5d9
belindaabusk <au615813@post.au.dk>
2020-05-18 10:41:33 +0200

    .

--- Projektet/EKGOversigten.xaml.cs
+++ Projektet/EKGOversigten.xaml.cs
@@ -18,113 +18,113 @@ using LogicLayer;
 
 namespace Projektet_GUI
 {
-   /// <summary>
-   /// Interaction logic for EKGOversigten.xaml
-   /// </summary>
+    /// <summary>
+    /// Interaction logic for EKGOversigten.xaml
+    /// </summary>
 
-   public partial class EKGOversigten : Window
-   {
-      public ChartValues<double> Ekgvalues { get; set; }
-      public string[] LabelsY { get; set; }
-      private List<Patient> PatientListe;
-      private Logic logicref;
-      private List<EKG_Maaling> MaalingListe;
-      private EKG_Maaling Maaling;
-      private Patient patient1;
+    public partial class EKGOversigten : Window
+    {
+        public ChartValues<double> Ekgvalues { get; set; }
+        public string[] LabelsY { get; set; }
+        private List<Patient> PatientListe;
+        private Logic logicref;
+        private List<EKG_Maaling> MaalingListe;
+        private EKG_Maaling Maaling;
+        private Patient patient1;
 
-      public EKGOversigten(Logic logicref)
-      {
-         InitializeComponent();
-         this.logicref = logicref;
-      }
+        public EKGOversigten(Logic logicref)
+        {
+            InitializeComponent();
+            this.logicref = logicref;
+        }
 
-      private void Window_Loaded(object sender, RoutedEventArgs e)
-      {
-         PatientListe = logicref.getPatientListe();
-         foreach (Patient item in PatientListe)
-         {
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PatientListe = logicref.getPatientListe();
+            foreach (Patient item in PatientListe)
+            {
 
-            PatientLB.Items.Add(item.Navn + " " + item.Efternavn);
-         }
+                PatientLB.Items.Add(item.Navn + " " + item.Efternavn);
+            }
 
-         fnTB.Text = "Søren";
-         EfTB.Text = "Pedersen";
-         MedarnrTB.Text = "123";
-      }
+            fnTB.Text = "Søren";
+            EfTB.Text = "Pedersen";
+            MedarnrTB.Text = "123";
+        }
 
-      private void GemB_Click(object sender, RoutedEventArgs e)
-      {
+        private void GemB_Click(object sender, RoutedEventArgs e)
+        {
             MessageBox.Show("EKG-Målingen er nu afsendt til offentlig database");
-         MaalingListe = logicref.getMaalingListe(PatCprTB.Text);
-         patient1 = logicref.getCPR(PatCprTB.Text);
+            MaalingListe = logicref.getMaalingListe(PatCprTB.Text);
+            patient1 = logicref.getCPR(PatCprTB.Text);
 
-         int antalmaalinger = 0;
+            int antalmaalinger = 0;
 
-         foreach (EKG_Maaling item in MaalingListe)
-         {
-            antalmaalinger++;
-            if (Convert.ToDateTime(DatoLB.SelectedItem) == item.Starttid)
+            foreach (EKG_Maaling item in MaalingListe)
             {
+                antalmaalinger++;
+                if (Convert.ToDateTime(DatoLB.SelectedItem) == item.Starttid)
+                {
 
-               logicref.gemIoffentligDatabase(Convert.ToDateTime(DatoLB.SelectedItem), antalmaalinger, fnTB.Text, EfTB.Text, Convert.ToInt32(MedarnrTB.Text), orgTB.Text, KommentarTB.Text, patient1.Navn, patient1.Efternavn, PatCprTB.Text, Maaling);
+                    logicref.gemIoffentligDatabase(Convert.ToDateTime(DatoLB.SelectedItem), antalmaalinger, fnTB.Text, EfTB.Text, Convert.ToInt32(MedarnrTB.Text), orgTB.Text, KommentarTB.Text, patient1.Navn, patient1.Efternavn, PatCprTB.Text, Maaling);
 
+                }
             }
-         }
 
-      }
+        }
 
-      private void PatientLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
-      {
+        private void PatientLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
             DatoLB.Items.Clear();
-         foreach (Patient item in PatientListe)
-         {
-            if (Convert.ToString(PatientLB.SelectedItem) == item.Navn + " " + item.Efternavn)
+            foreach (Patient item in PatientListe)
             {
-               PatCprTB.Text = item.CPR;
+                if (Convert.ToString(PatientLB.SelectedItem) == item.Navn + " " + item.Efternavn)
+                {
+                    PatCprTB.Text = item.CPR;
+                }
             }
-         }
 
-         MaalingListe = logicref.getMaalingListe(PatCprTB.Text);
+            MaalingListe = logicref.getMaalingListe(PatCprTB.Text);
 
-         foreach (EKG_Maaling item in MaalingListe)
-         {
-            DatoLB.Items.Add(item.Starttid);
-         }
+            foreach (EKG_Maaling item in MaalingListe)
+            {
+                DatoLB.Items.Add(item.Starttid);
+            }
 
-      }
+        }
 
-      private void DatoLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
-      {
+        private void DatoLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
             Ekgvalues = new ChartValues<double>();
 
             foreach (EKG_Maaling item in MaalingListe)
-         {
-            if (item.Starttid == Convert.ToDateTime(DatoLB.SelectedItem))
-               Maaling = logicref.sygdomsalgoritme_Måling(item.CPR, item.Starttid);
-         }
+            {
+                if (item.Starttid == Convert.ToDateTime(DatoLB.SelectedItem))
+                    Maaling = logicref.sygdomsalgoritme_Måling(item.CPR, item.Starttid);
+            }
 
-         Ekgvalues = new ChartValues<double>();
-         DataContext = this;
-         LabelsY = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
-
-  
-         foreach (double item in Maaling.EKG_Data)
-         {
-            Ekgvalues.Add(item);
-         }
-
-         if (Maaling.Sygdom == true)
-         {
-            sygdomTB.Text = "Ja";
-         }
-         else
-         {
-            sygdomTB.Text = "Nej";
-         }
+            Ekgvalues = new ChartValues<double>();
+            DataContext = this;
+            LabelsY = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
 
 
-      }
+            foreach (double item in Maaling.EKG_Data)
+            {
+                Ekgvalues.Add(item);
+            }
 
-   }
+            if (Maaling.Sygdom == true)
+            {
+                sygdomTB.Text = "Ja";
+            }
+            else
+            {
+                sygdomTB.Text = "Nej";
+            }
+
+
+        }
+
+    }
 
 }
